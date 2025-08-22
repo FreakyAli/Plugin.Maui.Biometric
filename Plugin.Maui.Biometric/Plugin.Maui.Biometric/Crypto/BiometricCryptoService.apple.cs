a0e9f009ac75ec4392c08bb3305be3ea14347a81
Ali <31090457+FreakyAli@users.noreply.github.com>
2025-08-22 12:53:34 +0530

    fix: cleaned up

--- Plugin.Maui.Biometric/Plugin.Maui.Biometric/Crypto/BiometricCryptoService.apple.cs
+++ Plugin.Maui.Biometric/Plugin.Maui.Biometric/Crypto/BiometricCryptoService.apple.cs
@@ -1,10 +1,10 @@
 namespace Plugin.Maui.Biometric;
-partial class BiometricCryptoService
+internal partial class BiometricCryptoService
 {
-    public partial Task CreateKeyAsync(string keyId, CryptoKeyOptions options, CancellationToken token = default)
-    {
+    public partial Task CreateKeyAsync(string keyId, CryptoKeyOptions options, CancellationToken token)
+    { 
         return Task.CompletedTask;
-    }
+    }  
 
     public partial Task DeleteKeyAsync(string keyId)
     {
@@ -16,27 +16,27 @@ partial class BiometricCryptoService
         return Task.FromResult(false);
     }
 
-    public partial Task<SecureCryptoResponse> DecryptAsync(string keyId, byte[] inputData, CancellationToken token = default)
+    public partial Task<SecureCryptoResponse> DecryptAsync(string keyId, byte[] inputData, CancellationToken token)
     {
         return Task.FromResult(new SecureCryptoResponse());
     }
 
-    public partial Task<SecureCryptoResponse> EncryptAsync(string keyId, byte[] inputData, CancellationToken token = default)
+    public partial Task<SecureCryptoResponse> EncryptAsync(string keyId, byte[] inputData, CancellationToken token)
+    {
+        return Task.FromResult(new SecureCryptoResponse());
+    }   
+
+    public partial Task<SecureCryptoResponse> MacAsync(string keyId, byte[] inputData, CancellationToken token)
     {
         return Task.FromResult(new SecureCryptoResponse());
     }
 
-    public partial Task<SecureCryptoResponse> MacAsync(string keyId, byte[] inputData, CancellationToken token = default)
+    public partial Task<SecureCryptoResponse> SignAsync(string keyId, byte[] inputData, CancellationToken token)
     {
         return Task.FromResult(new SecureCryptoResponse());
     }
 
-    public partial Task<SecureCryptoResponse> SignAsync(string keyId, byte[] inputData, CancellationToken token = default)
-    {
-        return Task.FromResult(new SecureCryptoResponse());
-    }
-
-    public partial Task<SecureCryptoResponse> VerifyAsync(string keyId, byte[] inputData, byte[] signature, CancellationToken token = default)
+    public partial Task<SecureCryptoResponse> VerifyAsync(string keyId, byte[] inputData, byte[] signature, CancellationToken token)
     {
         return Task.FromResult(new SecureCryptoResponse());
     }
